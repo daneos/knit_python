02f22cec311ca11f16919d91b29d54175e71ac6a
daneos <daneos@daneos.com>
2015-06-02 23:18:45 +0200

    zad4

---
+++ zadania1/4/absDistinct.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+def absDistinct(A):
+	""" Ilosc unikalnych wartosci bezwzglednych elementow listy """
+	return len(set( [abs(x) for x in A] ))		# uzywam zbioru do usuniecia powtarzajacych sie elementow
+
+
+#------------------------------------------------------------------------------
+if __name__ == "__main__":
+	A = [ -5, -3, -1, 0, 3, 6 ]
+	print absDistinct(A)
