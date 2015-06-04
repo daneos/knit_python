53d6a7af52911d8964d09e566366c58a9789108c
daneos <daneos@daneos.com>
2015-06-04 16:41:07 +0200

    it's something!

--- zadania1/5/rest/rest/PersonalLog.py
+++ zadania1/5/rest/rest/PersonalLog.py
@@ -2,6 +2,7 @@ from rest_framework.views import View
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
+from time import time
  
 log = [
 	{ "category":"update", "person":"all", "time":1433412540, "content":"testtesttest test test!" },
@@ -10,27 +11,32 @@ log = [
 	{ "category":"update", "person":"all", "time":1433412777, "content":"testtesttest time test!" },
 	{ "category":"update", "person":"john", "time":1433412540, "content":"testtesttest test john" }
 ]
- 
-class Log(View):
-	@api_view(['GET'])
-	def get(self, request):
+
+@api_view(["GET", "POST"])
+def home(request):
+	if request.method == "GET":
 		print "Log matched"
 		return Response(log)
+	elif request.method == "POST":
+		log.append({
+			"category":	"test",
+			"person":	"test2",
+			"time":		int(time()),
+			"content":	request.data
+			})
+		return Response(status=status.HTTP_201_CREATED)
 
-class LogByCategory(View):
-	@api_view(['GET'])
-	def get(self, request, cat):
-		print "LogByCategory matched"
+@api_view(['GET'])
+def getByCategory(request, cat):
+	if request.method == "GET":
 		return Response([ x for x in log if x["category"] == cat ])
  
-class LogByPerson(View):
-	@api_view(['GET'])
-	def get(self, request, per):
-		print "LogByPerson matched"
+@api_view(['GET'])
+def getByPerson(request, per):
+	if request.method == "GET":
 		return Response([ x for x in log if x["person"] == per ])
 
-class LogByTime(View):
-	@api_view(['GET'])
-	def get(self, request, time):
-		print "LogByTime matched"
-		return Response([ x for x in log if x["time"] == time])
+@api_view(['GET'])
+def getByTime(request, time):
+	if request.method == "GET":
+		return Response([ x for x in log if x["time"] == int(time) ])
--- zadania1/5/rest/rest/urls.py
+++ zadania1/5/rest/rest/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import patterns, include, url
-from PersonalLog import *
+import rest.PersonalLog
 
 urlpatterns = patterns("",
-	url(r"^$", Log.as_view()),
-	url(r"^category/(?P<cat>[A-Za-z0-9]*)$", LogByCategory.as_view()),
-	url(r"^person/(?P<per>[A-Za-z0-9]*)$", LogByPerson.as_view()),
-	url(r"^time/(?P<time>[0-9]*)$", LogByTime.as_view()),
+	url(r"^$", rest.PersonalLog.home),
+	url(r"^category/(?P<cat>[A-Za-z0-9]*)$", rest.PersonalLog.getByCategory),
+	url(r"^person/(?P<per>[A-Za-z0-9]*)$", rest.PersonalLog.getByPerson),
+	url(r"^time/(?P<time>[0-9]*)$", rest.PersonalLog.getByTime),
 )
