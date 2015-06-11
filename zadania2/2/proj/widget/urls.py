72e682c471c1291af1febbde9ba0bc100a09ae42
daneos <daneos@daneos.com>
2015-06-12 00:29:55 +0200

    cos zaczyna dzialac

--- zadania2/2/proj/widget/urls.py
+++ zadania2/2/proj/widget/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import include, url
 
 urlpatterns = [
-	url(r'^(?P<device>.+)', 'widget.show'),
+	url(r'^(?P<device>.+)', 'widget.views.show'),
 ]
--- zadania2/2/proj/widget/views.py
+++ zadania2/2/proj/widget/views.py
@@ -5,5 +5,16 @@ def show(request, device):
 	""" Widok widgetu - pokazuje atrybuty urzadzenia """
 	d = Device(device)
 
-	for i in range(0, d.attList.attCount-1):
-		pass
+	attrs = []
+	for i in range(1, d.attList['attCount']):
+		attrs.append({
+			'Name':		d.attList['attribute' + str(i-1)],
+			'Scalar':	d.attList['attScalar' + str(i-1)],
+			'Value':	d.attList['attValue' + str(i-1)],
+			'Writable':	d.attList['attWritable' + str(i-1)],
+			'Plotable':	d.attList['attPlotable' + str(i-1)],
+			'Numeric':	d.attList['isNumeric' + str(i-1)],
+			'Desc':		d.attList['attDesc' + str(i-1)]
+		})
+	context = { 'device': device, 'attrs': attrs, 'connection': d.attList['connectionStatus'] }
+	return render_to_response('widget/show.template.html', context)
