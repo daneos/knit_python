8e5476a737c0957c3b136ca499834db16833150f
daneos <daneos@daneos.com>
2015-06-11 22:36:19 +0200

    komentarze

--- zadania2/1/proj/polls/views.py
+++ zadania2/1/proj/polls/views.py
@@ -6,32 +6,35 @@ from polls.models import Choice, Poll
 
 def index(request):
 	""" Strona glowna - lista ankiet """
-	latest_poll_list = Poll.objects.all().order_by('-date')
+	latest_poll_list = Poll.objects.all().order_by('-date')		# sortowanie od najnowszego
 	return render_to_response('polls/index.template.html', {'latest_poll_list': latest_poll_list})
 
 def detail(request, poll_id):
-	""" Widok  szczegolowy ankiety - formularz glosowania """
+	""" Widok szczegolowy ankiety - formularz glosowania """
 	p = get_object_or_404(Poll, pk=poll_id)
 	context = {'poll': p}
-	context.update(csrf(request))
+	context.update(csrf(request))		# ochrona przed CSRF
 	return render_to_response('polls/detail.template.html', context)
 
 def vote(request, poll_id):
 	""" Obsluga glosow """
-	p = get_object_or_404(Poll, pk=poll_id)
-	try:
-		selected_choice = p.choice_set.get(pk=request.POST['choice'])
-	except (KeyError, Choice.DoesNotExist):
-		context = {
-			'poll': p,
-			'error_message': "Nie wybrales zadnej opcji",
-		}
-		context.update(csrf(request))
-		return render_to_response('polls/detail.template.html', context)
+	if request.method == 'POST':
+		p = get_object_or_404(Poll, pk=poll_id)
+		try:
+			selected_choice = p.choice_set.get(pk=request.POST['choice'])
+		except (KeyError, Choice.DoesNotExist):
+			context = {
+				'poll': p,
+				'error_message': "No choice was selected",
+			}
+			context.update(csrf(request))	# ochrona przed CSRF
+			return render_to_response('polls/detail.template.html', context)
+		else:
+			selected_choice.votes += 1
+			selected_choice.save()			# zapis oddanego glosu
+			return HttpResponseRedirect(reverse('polls.views.results', args=(p.id,)))
 	else:
-		selected_choice.votes += 1
-		selected_choice.save()
-		return HttpResponseRedirect(reverse('polls.views.results', args=(p.id,)))
+		return detail(request, poll_id)		# jesli metoda nie jest POST zwraca widok ankiety
 
 def results(request, poll_id):
 	""" Widok wynikow ankiety """
